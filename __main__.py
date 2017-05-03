06d4c5f3ee4818349e94266df9c1de1de0cb696c
bence-takacs <takacs.bence@gmail.com>
2017-05-03 14:23:10 +0200

    __init__ to __main__

--- __main__.py
+++ __main__.py
@@ -1,4 +1,1 @@
-import feed_parser.FeedParser
-
-logging.basicConfig(format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s", filename='fp.log',
-                            level=logging.DEBUG)
+import feed_parser
